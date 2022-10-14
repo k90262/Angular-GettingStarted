e26a1606e51e3c1904bcf05832b4361c64236881
Bill Ho <ycho.bill.lab@gmail.com>
2022-10-14 15:13:23 +0800

    [ch.6] Transform Data with Pipes

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -30,6 +30,16 @@ export class ProductListComponent {
             "price": 32.99,
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
+          },
+          {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
           }
     ];
 
