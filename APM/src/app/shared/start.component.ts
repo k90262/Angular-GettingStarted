692a72660bf88c06a34b7f0f478952b0646b282b
Bill Ho <ycho.bill.lab@gmail.com>
2022-10-21 17:28:21 +0800

    [ch.8] (1. Building, and (2. Using a Nested Component, (3. Passing Data to a Nested Component

---
+++ APM/src/app/shared/start.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnChanges } from "@angular/core";
+
+@Component({
+    selector: "pm-star",
+    templateUrl: "./star.component.html",
+    styleUrls: ["./star.component.css"]
+})
+export class StarComponent implements OnChanges {
+    cropWidth: number = 75;
+    @Input()
+    rating: number = 0;
+    ngOnChanges(): void {
+        this.cropWidth = this.rating * 75/5;
+    }
+}
