622235e8474f1d148559d5cc8517fba95362bef2
Bill Ho <ycho.bill.lab@gmail.com>
2022-12-16 23:19:42 +0800

    [ch.13] Shared Module. (using angular cli to create and import: ng g m shared/shared --flat -m products/product.module)

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/start.component';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { 
@@ -26,7 +21,8 @@ import { FormsModule } from '@angular/forms';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent 
       }
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
